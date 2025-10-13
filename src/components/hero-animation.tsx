2a226ba5600e915fd8efa4b6713fcbe8c5dd67c3
Jithendra <jith05122005@gmail.com>
2025-10-13 16:42:23 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/components/hero-animation.tsx
+++ src/components/hero-animation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Lottie from 'lottie-react';
-import animationData from '../../../public/animations/hero-animation.json';
+import animationData from '@/../public/animations/hero-animation.json';
 
 const HeroAnimation = () => {
   return <Lottie animationData={animationData} loop={true} className="w-full max-w-md mx-auto" />;
