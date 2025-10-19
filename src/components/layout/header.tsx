4887d1cfd1479e65fe25cf1ef5c332a87d25ada5
Jithendra <jith05122005@gmail.com>
2025-10-19 15:57:25 +0000

    make sure that even in log tab it should be visible

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,9 +9,8 @@ import { ThemeToggle } from '../theme-toggle';
 export default function AppHeader({ user }: { user: AppUser | null }) {
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6">
-      <div className="md:hidden">
-        <SidebarTrigger />
-      </div>
+      {/* This SidebarTrigger is now visible on all screen sizes */}
+      <SidebarTrigger />
       <div className="flex w-full items-center justify-end gap-4">
         <ThemeToggle />
         {!user ? (
