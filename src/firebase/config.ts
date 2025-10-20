4c1df18dbaa47f060533c181f63c3df4c5c1e489
Jithendra <jith05122005@gmail.com>
2025-10-20 08:57:51 +0000

    why image anylisis and duplicationis working this firebase studio runned

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7951954097-a1748",
   "appId": "1:1080617705267:web:f8a351cc90e7c482d96846",
+  "storageBucket": "studio-7951954097-a1748.appspot.com",
   "apiKey": "AIzaSyD_lMZE-yhubDlW8swX1m4uu8OtGvXCe0I",
   "authDomain": "studio-7951954097-a1748.firebaseapp.com",
   "measurementId": "",
